22411e077af982ae10a21a0b7807db3d1d3ba9ee
Maniackaa <maniac_kaa@mail.ru>
2023-09-11 09:01:55 +0700

    fix

--- backend_deposit/backend_deposit/settings.py
+++ backend_deposit/backend_deposit/settings.py
@@ -9,9 +9,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = (os.getenv('DEBUG', 'False').lower() == 'true')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
-
+CSRF_TRUSTED_ORIGINS = ['https://*.mydomain.com','https://*.127.0.0.1','http://194.87.252.190','https://194.87.252.190']
 
 MY_APPS = [
+    'users.apps.UsersConfig',
     'deposit.apps.DepositConfig',
 ]
 
@@ -103,7 +104,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
