3164544ff8d6bdb8f9c6362bf68b4f55f28abf93
Maniackaa <maniac_kaa@mail.ru>
2023-09-12 13:05:41 +0700

    add volume ftp

--- ftp/ftp_sender.py
+++ ftp/ftp_sender.py
@@ -1,26 +1,33 @@
+import time
+
 import requests
 
 from config_data.bot_conf import get_my_loggers
 from ftp.config_data.ftp_conf import BASE_DIR
 
 path = BASE_DIR / 'screenshots'
-files = list(path.glob('*.jpg'))
+
 
 logger, err_log, *other = get_my_loggers()
-print(other)
 
+files = list(path.glob('*.jpg'))
+print(files)
 for file in files:
+
     try:
-        logger.debug(f'{file.name, file.lstat().st_size}')
+        start = time.perf_counter()
+        logger.debug(f'Отправляем {file.name, file.lstat().st_size}')
         binary = open(file, "rb")
         screen = {'image': binary}
         response = requests.post("http://localhost/api/screen/", data={'name': file.name}, files=screen)
-        binary.close()
         logger.debug(f'{response, response.status_code}')
+        logger.debug(f'Время отправки: {time.perf_counter() - start}')
+        binary.close()
         content = response.json()
-        if response.status_code == 201:
+        if response.status_code in [200, 201]:
             file.unlink()
         else:
             pass
+        break
     except Exception as err:
         logger.error(err)
