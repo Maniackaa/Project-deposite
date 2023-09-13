c03ddf361604acf3c92e79ea00c1637b8136477c
Maniackaa <maniac_kaa@mail.ru>
2023-09-13 17:03:18 +0700

    fix bad

--- backend_deposit/deposit/views.py
+++ backend_deposit/deposit/views.py
@@ -12,7 +12,7 @@ from rest_framework.request import Request
 
 from backend_deposit.settings import LOGCONFIG
 from deposit.func import img_path_to_str
-from deposit.models import BadScreen
+from deposit.models import BadScreen, Incoming
 from deposit.screen_response import screen_text_to_pay
 from deposit.serializers import IncomingSerializer
 
@@ -39,32 +39,33 @@ def screen(request: Request):
     sms_type = pay.get('type')
     transaction = pay.get('transaction')
     if pay_status.lower() != 'успешно' and sms_type != '':
-        print('trash')
+        logger.debug('trash')
         is_duplicate = BadScreen.objects.filter(transaction=transaction).exists()
         if not is_duplicate:
+            logger.debug('Мусор не дубликат')
             BadScreen.objects.create(name=name, worker=worker, image=image, transaction=transaction, type=sms_type)
             return HttpResponse(status=status.HTTP_200_OK,
                                 reason='BadScreen',
                                 charset='utf-8')
         else:
-            print('Дубликат в BadScreen')
+            logger.debug('Дубликат - уже есть в BadScreen')
             return HttpResponse(status=status.HTTP_200_OK,
                                 reason='duplicate in BadScreen',
                                 charset='utf-8')
 
-
     serializer = IncomingSerializer(data=pay)
     if serializer.is_valid():
-        print('valid')
+        logger.debug('valid')
         serializer.save(worker=worker, image=image)
         return HttpResponse(status=status.HTTP_201_CREATED,
                             reason='created',
                             charset='utf-8')
     else:
-        print('invalid')
-        print(serializer.errors)
-        is_duplicate = BadScreen.objects.filter(transaction=transaction).exists()
-        if not is_duplicate:
+        logger.debug('invalid')
+        logger.debug(serializer.errors)
+        is_duplicate_incoming = BadScreen.objects.filter(transaction=transaction).exists()
+        is_duplicate_trash = Incoming.objects.filter(transaction=transaction).exists()
+        if not is_duplicate_trash and not is_duplicate_incoming:
             BadScreen.objects.create(name=name, worker=worker, image=image, transaction=transaction, type=sms_type)
             return HttpResponse(status=status.HTTP_200_OK,
                                 reason='trash',
