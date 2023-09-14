720e3734d0bdb6a454b993ed0f5014b02e4d936b
Maniackaa <maniac_kaa@mail.ru>
2023-09-14 23:05:50 +0700

    inpit deposit

--- backend_deposit/entrypoint.sh
+++ backend_deposit/entrypoint.sh
@@ -3,4 +3,5 @@ sleep 1
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 python3 manage.py collectstatic  --noinput
+cp -RT static collected_static
 gunicorn --workers 3  --bind 0.0.0.0:$GUNICORN_PORT backend_deposit.wsgi
