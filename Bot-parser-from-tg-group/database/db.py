c03ec3ae2aa7f2bfa4f15214d13fdbd9e4394fc3
Maniackaa <maniac_kaa@mail.ru>
2023-10-02 22:28:45 +0700

    add alembic

--- Bot-parser-from-tg-group/database/db.py
+++ Bot-parser-from-tg-group/database/db.py
@@ -20,7 +20,7 @@ err_log = logging.getLogger('errors_logger')
 metadata = MetaData()
 db_url = f"postgresql+psycopg2://{conf.db.db_user}:{conf.db.db_password}@{conf.db.db_host}:{conf.db.db_port}/{conf.db.database}"
 engine = create_engine(db_url, echo=False)
-from sqlalchemy_utils.functions import database_exists, create_database
+# from sqlalchemy_utils.functions import database_exists, create_database
 
 Session = sessionmaker(bind=engine)
 
@@ -31,7 +31,7 @@ class Base(DeclarativeBase):
 
 class Incoming(Base):
     __tablename__ = 'deposit_incoming'
-    __table_args__ = (UniqueConstraint("response_date", "pay", "sender", name="unique_sms"),)
+    # __table_args__ = (UniqueConstraint("response_date", "pay", "sender", name="unique_sms"),)
     id: Mapped[int] = mapped_column(primary_key=True,
                                     autoincrement=True,
                                     comment='Первичный ключ')
@@ -60,8 +60,8 @@ class TrashIncoming(Base):
     text: Mapped[str] = mapped_column(Text())
 
 
-if not database_exists(db_url):
-    create_database(db_url)
+# if not database_exists(db_url):
+#     create_database(db_url)
 
 Base.metadata.create_all(engine)
 
